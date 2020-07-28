1200190e8b0bb80ae182bd18633d8641ca67dfa0
Yx_Zou <hello.yxhome@gmail.com>
2020-07-28 23:18:17 +0800

    完成一个简单的 webpack 打包测试

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@
 
 const path = require('path')
 
+// .\node_modules\.bin\webpack  运行无需加 cd 
 module.exports = {
     entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+    output: { 
+        path: path.join(__dirname, 'dist'), // 输出文件夹
+        filename: 'bundle.js' // 打包的文件名
     },
     mode: 'production'
 }
