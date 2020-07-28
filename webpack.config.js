cf8fa29f0d399977b15345c3189829f33061a66e
Yx_Zou <hello.yxhome@gmail.com>
2020-07-28 23:35:28 +0800

    entry 配置介绍

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,18 @@ const path = require('path')
 
 // .\node_modules\.bin\webpack  运行无需加 cd 
 module.exports = {
+    // entry 指定 webpack 的打包入口
+
+    /**
+     * // entry 多入口 是一个对象
+     * entry:{
+     *  app：'./src/app.js',
+     *  adminApp：'./src/adminApp.js',
+     * }
+     *  */
+    // entry 单入口是一个字符串
     entry: './src/index.js',
-    output: { 
+    output: {
         path: path.join(__dirname, 'dist'), // 输出文件夹
         filename: 'bundle.js' // 打包的文件名
     },
